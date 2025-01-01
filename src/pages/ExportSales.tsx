cb7227ebd8b3aecad8767f3c33df5190f3c792e5
Feareis <v.reisse@protonmail.com>
2025-01-01 16:41:04 +0100

    Update ExportSales.tsx

--- src/pages/ExportSales.tsx
+++ src/pages/ExportSales.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const ExportSales: React.FC = () => {
-  return <div className="text-center text-xl font-bold">Export Sales Page</div>;
+  return (
+      <div className="text-center text-xl font-bold">Export Sales Page</div>
+    );
 };
 
 export default ExportSales;
