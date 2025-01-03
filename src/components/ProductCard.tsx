24f245316565a8c9cd1306335d5f735b550a5a33
Feareis <v.reisse@protonmail.com>
2025-01-03 05:57:42 +0100

    v0.6.8
    Add more responsive items

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,8 +4,8 @@ interface ProductCardProps {
   name: string;
   image: string;
   quantity: number;
-  increments: number[]; // Tableau d'incréments, ex : [1, 10]
-  decrements: number[]; // Tableau de décréments, ex : [10, 1]
+  increments: number[];
+  decrements: number[];
   onIncrement: (value: number) => void;
   onDecrement: (value: number) => void;
   onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -22,21 +22,21 @@ const ProductCard: React.FC<ProductCardProps> = ({
   onInputChange,
 }) => {
   return (
-    <div className="bg-gray-800 shadow-lg rounded-lg p-4 flex flex-col items-center transition duration-200 hover:scale-105">
-      <h3 className="text-2xl font-semibold text-white mb-2">{name}</h3>
-      <div className="w-full border-t border-gray-500 my-2"></div>
+    <div className="bg-gray-800 shadow-lg rounded-lg p-4 flex flex-col items-center transition-transform duration-200 hover:scale-105 sm:p-6 w-full max-w-sm">
+      <h3 className="text-xl sm:text-2xl font-semibold text-center text-white mb-2 w-full truncate overflow-hidden text-ellipsis">{name}</h3>
+      <div className="w-full border-t border-gray-500 my-2 sm:my-4"></div>
       <img
         src={image}
         alt={name}
-        className="w-4/6 h-full object-cover rounded-md mb-4"
+        className="w-3/4 sm:w-2/3 lg:w-1/2 h-auto object-contain rounded-md mb-4"
       />
-      <div className="w-full border-t border-gray-500 my-2"></div>
-      <div className="flex items-center gap-2 px-4 py-2">
+      <div className="w-full border-t border-gray-500 my-2 sm:my-4"></div>
+      <div className="flex items-center justify-between gap-2 w-full">
         {decrements.map((value) => (
           <button
             key={`decrement-${value}`}
             onClick={() => onDecrement(value)}
-            className="text-red-500 font-semibold hover:text-red-600 px-2"
+            className="text-red-500 font-semibold hover:text-red-600 px-3"
           >
             -{value}
           </button>
@@ -45,13 +45,13 @@ const ProductCard: React.FC<ProductCardProps> = ({
           type="text"
           value={quantity}
           onChange={onInputChange}
-          className="w-20 text-center bg-gray-200 rounded-lg p-1.5 mx-2 appearance-none"
+          className="w-16 text-center bg-gray-200 rounded-lg p-1.5 appearance-none"
         />
         {increments.map((value) => (
           <button
             key={`increment-${value}`}
             onClick={() => onIncrement(value)}
-            className="text-green-500 font-semibold hover:text-green-600 px-2"
+            className="text-green-500 font-semibold hover:text-green-600 px-3"
           >
             +{value}
           </button>
--- src/pages/ClientsSales.tsx
+++ src/pages/ClientsSales.tsx
@@ -304,7 +304,7 @@ const ClientsSales: React.FC = () => {
             />
 
             {/* Produits */}
-            <div className="text-gray-700 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
+            <div className="text-gray-700 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
               {items
                 .filter((item) => item.category === activeTab)
                 .map((item) => (
