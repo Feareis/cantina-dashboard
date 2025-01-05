13eb9c476f89eb9fc4c30b3ac69e36749db7f24c
Feareis <v.reisse@protonmail.com>
2025-01-05 11:49:10 +0100

    v9.0.8

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,6 +4,7 @@ const Profile: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <h3 className="text-3xl font-bold bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">Profile Page</h3>
+      <p className="text-xl font-semibold text-white">Informations générales sur l'utilisateur</p>
     </div>
   );
 };
