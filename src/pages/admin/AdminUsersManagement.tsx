a0d2d2f95fb9f117831337401eac30b2db1b8e12
Feareis <v.reisse@protonmail.com>
2025-01-17 22:13:16 +0100

    Update AdminUsersManagement.tsx

--- src/pages/admin/AdminUsersManagement.tsx
+++ src/pages/admin/AdminUsersManagement.tsx
@@ -9,9 +9,7 @@ type User = {
   role: string;
   is_active: boolean;
   employee_id: string;
-  employees?: {
-    grade: string;
-  };
+  employees: { grade: string }[];
 };
 
 const AdminUsersManagement: React.FC = () => {
