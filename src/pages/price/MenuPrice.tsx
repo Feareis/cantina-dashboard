85a31576472318cf225aba4dd604aeb989bf8710
Feareis <v.reisse@protonmail.com>
2025-01-17 21:46:51 +0100

    Update MenuPrice.tsx

--- src/pages/price/MenuPrice.tsx
+++ src/pages/price/MenuPrice.tsx
@@ -1,16 +1,5 @@
 import React from "react";
-import ProductCardPrice from "./../../components/card/ProductCardPrice";
 
-const products = [
-  {
-    id: 1,
-    name: "Produit A",
-    priceClean: 25.99,
-    priceDirty: 19.99,
-    additionalInfo: "Description du produit A, fabriqué avec soin.",
-  },
-  // Ajoutez d'autres produits ici
-];
 
 const MenuPrice: React.FC = () => {
   return (
